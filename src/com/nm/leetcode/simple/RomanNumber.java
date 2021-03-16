9876744942555d6627648f985b481bf75fdca45f
ningmin <603565766@qq.com>
2021-03-16 13:18:16 +0800

    初始化+罗马数字转数字题

---
+++ src/com/nm/leetcode/simple/RomanNumber.java
@@ -0,0 +1,102 @@
+package com.nm.leetcode.simple;
+
+/**
+ * 罗马数字转数字
+ * @Author NM
+ * @Date 2021/3/16 12:34
+ */
+public class RomanNumber {
+
+    public static void main(String[] args) {
+      int i = new RomanNumber().romanToInt("MCMXCIV");
+      System.out.println(i);
+    }
+
+//    I             1
+//    V             5
+//    X             10
+//    L             50
+//    C             100
+//    D             500
+//    M             1000
+//    I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+//    X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+//    C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+
+    public int romanToInt(String s) {
+        return calc(s, 0);
+    }
+
+    private int calc(String s, int result) {
+        if(s.length() == 0){
+            return result;
+        }else{
+          int flag = 1;
+          String[] subArr = s.split("");
+          String first = subArr[0];
+          if("M".equals(first)){
+              result+=1000;
+          }
+          else if("D".equals(first)){
+              result+=500;
+          }
+          else if("C".equals(first)){
+              if(subArr.length == 1){
+                  result+=100;
+              }else{
+                  String next = subArr[1];
+                  if("M".equals(next)){
+                      result+=900;
+                      flag++;
+                  }else if("D".equals(next)){
+                      result+=400;
+                      flag++;
+                  }else{
+                      result+=100;
+                  }
+              }
+          }
+          else if("L".equals(first)){
+              result+=50;
+          }
+          else if("X".equals(first)){
+              if(subArr.length == 1){
+                  result+=10;
+              }else {
+                  String next = subArr[1];
+                  if ("C".equals(next)) {
+                      result += 90;
+                      flag++;
+                  } else if ("L".equals(next)) {
+                      result += 40;
+                      flag++;
+                  } else {
+                      result += 10;
+                  }
+              }
+          }
+          else if("V".equals(first)){
+              result+=5;
+          }
+          else if("I".equals(first)){
+              if(subArr.length == 1){
+                  result+=1;
+              }else{
+                  String next = subArr[1];
+                  if("X".equals(next)){
+                      result+=9;
+                      flag++;
+                  }else if("V".equals(next)){
+                      result+=4;
+                      flag++;
+                  }else{
+                      result+=1;
+                  }
+              }
+          }
+          return calc(s.substring(flag), result);
+        }
+
+    }
+
+}
